42844a30a8dec0c88fe96c724882e60b361f260c
Géraud <gbeguin@xebia.fr>
2016-01-09 20:22:51 +0100

    Extracted match day strings to resources file

--- Football_Scores/app/src/main/java/barqsoft/footballscores/ScoresAdapter.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/ScoresAdapter.java
@@ -67,10 +67,10 @@ public class ScoresAdapter extends CursorAdapter
         container.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT
                 , ViewGroup.LayoutParams.MATCH_PARENT));
         TextView match_day = (TextView) v.findViewById(R.id.matchday_textview);
-        match_day.setText(Utilities.getMatchDay(cursor.getInt(COL_MATCHDAY),
+        match_day.setText(Utilities.getMatchDay(context, cursor.getInt(COL_MATCHDAY),
                 cursor.getInt(COL_LEAGUE)));
         TextView league = (TextView) v.findViewById(R.id.league_textview);
-        league.setText(context.getString(Utilities.getLeague(cursor.getInt(COL_LEAGUE))));
+        league.setText(Utilities.getLeague(context, cursor.getInt(COL_LEAGUE)));
         Button share_button = (Button) v.findViewById(R.id.share_button);
         share_button.setOnClickListener(new View.OnClickListener() {
             @Override
